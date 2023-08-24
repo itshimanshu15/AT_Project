6a07fdf5ac026b2bc43202309611f4336d6126dd
itshimanshu15 <85964471+itshimanshu15@users.noreply.github.com>
2023-08-24 12:09:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,13 @@ const app = express();
 const socket = require("socket.io");
 require("dotenv").config();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://at-project-front.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  }
+));
 app.use(express.json());
 
 mongoose
