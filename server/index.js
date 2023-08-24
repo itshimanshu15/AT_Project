fa4ade074139688aa77d6cc4d63e19fb0c83bc32
itshimanshu15 <85964471+itshimanshu15@users.noreply.github.com>
2023-08-24 11:55:24 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "https://at-project.vercel.app",
+    origin: "https://at-project-front.vercel.app/login",
     methods: ["POST", "GET"],
     credentials: true,
   },
